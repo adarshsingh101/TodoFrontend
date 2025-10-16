42117fb3f760b1d9419cdd4c6861a644899c0b22
adarshsingh101 <adarshjisinghvishnu@gmail.com>
2025-10-16 12:05:36 +0530

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/todos/';
+const API_URL = 'https://todobackend-588l.onrender.com/api/todos/';
 
 const api = axios.create({
   baseURL: API_URL,
